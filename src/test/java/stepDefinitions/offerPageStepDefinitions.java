5e6d3e5ee3fc5187d07e418148a6c4ca72e258f4
cvir3 <vir3.chauhan@gmail.com>
2024-04-10 21:49:15 +0530

    Page Object Pattern V02

--- src/test/java/stepDefinitions/offerPageStepDefinitions.java
+++ src/test/java/stepDefinitions/offerPageStepDefinitions.java
@@ -3,6 +3,8 @@ package stepDefinitions;
 import io.cucumber.java.en.Then;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import pageObject.LandingPage;
+import pageObject.OffersPage;
 import utilities.TestContextSetup;
 
 import java.util.Iterator;
@@ -17,20 +19,22 @@ public class offerPageStepDefinitions {
 
     @Then("User searched for {string} shortname in offer page")
     public void user_searched_for_same_shortname_in_offer_page(String shortname) throws InterruptedException {
-        testContextSetup.driver.findElement(By.linkText("Top Deals")).click();
+        switchToOfferPage();
+        OffersPage op = new OffersPage(testContextSetup.driver);
+        op.searchItem(shortname);
+        Thread.sleep(3000);
+        testContextSetup.OfferPageProductName = op.getProductName();
+        System.out.println(testContextSetup.OfferPageProductName + "Is extracted from the home page ");
+    }
+    public void switchToOfferPage()
+    {
+        LandingPage lp = new LandingPage(testContextSetup.driver);
+        lp.selectTopDeals();
         Set<String> s1 = testContextSetup.driver.getWindowHandles();
         Iterator<String> i1 = s1.iterator();
         String parentWindow = i1.next();
         String childWindow = i1.next();
         testContextSetup.driver.switchTo().window(childWindow);
-        testContextSetup.driver.findElement(By.id("search-field")).sendKeys(shortname);
-        Thread.sleep(3000);
-        testContextSetup.OfferPageProductName = testContextSetup.driver.findElement(By.cssSelector("tr td:nth-child(1)")).getText();
-        System.out.println(testContextSetup.OfferPageProductName + "Is extracted from the home page ");
-    }
-    public void switchToWindows()
-    {
-
     }
 
     @Then("validate product name in offers page matches with Landing Page")
