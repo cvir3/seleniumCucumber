c37f5531e85bec31a6f5c57eac4b85d3359e2efc
Viren Chauhan <vir3.chauhan@gmail.com>
2024-07-12 00:29:26 +0530

    modified Readme file.

--- src/test/java/stepDefinitions/landingPageStepDefinition.java
+++ src/test/java/stepDefinitions/landingPageStepDefinition.java
@@ -3,7 +3,6 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import pageObject.LandingPage;
--- src/test/java/stepDefinitions/offerPageStepDefinitions.java
+++ src/test/java/stepDefinitions/offerPageStepDefinitions.java
@@ -1,7 +1,6 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.Then;
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import pageObject.LandingPage;
 import pageObject.OffersPage;
