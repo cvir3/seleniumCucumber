d11a50ecc1675d14964018386b19b38d1d9b9f04
Viren Chauhan <vir3.chauhan@gmail.com>
2024-07-16 16:19:19 +0530

    Generate Extent Spark html reports with screenshot

--- src/test/java/cucumberOption/TestNgRunner.java
+++ src/test/java/cucumberOption/TestNgRunner.java
@@ -5,8 +5,9 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = "src/test/java/features",
-        glue = "stepDefinitions", monochrome = true, tags="@PlaceOrder",
-        plugin = {"pretty", "html:target/cucumber.htm"}) // Plugin for report
+        glue = "stepDefinitions", monochrome = true, tags="@PlaceOrder or @SearchProduct  ",
+        plugin = {"pretty", "html:target/cucumber.htm",
+"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}) // Plugin for report
 public class TestNgRunner extends AbstractTestNGCucumberTests{
     @Override
     @DataProvider(parallel = true)
--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -48,5 +48,6 @@ public class TestBase {
             driver.get(url);
         }
         return driver;
+
     }
 }
